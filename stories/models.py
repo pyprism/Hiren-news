d6e63e9fbd92f09d8d048545c785f04ad5bbd1ea
pyprism <areose21@gmail.com>
2013-12-01 11:49:38 +0600

    update

--- stories/models.py
+++ stories/models.py
@@ -2,13 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from urlparse import urlparse
 
-class Stroy(models.Model):
+class Story(models.Model):
 	title = models.CharField(max_length=200)
 	url = models.URLField()
-	points = models.IntegerField()
+	points = models.IntegerField(default=0)
 	moderator = models.ForeignKey(User)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now_add=True)
+	
 	@property
 	def domain(self):
 		return urlparse(self.url).netloc
+
+	def __unicode__(self):
+		return self.title
+
+	class Meta:
+		verbose_name_plural = 'stories'
