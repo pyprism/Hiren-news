25a7e857474cc66d6c9da4b445148f70171102e6
pyprism <git.pyprism@gmail.com>
2014-06-08 19:32:40 +0600

    working scrapper completed

---
+++ newsgrabber/hiren.py
@@ -0,0 +1,34 @@
+__author__ = 'prism'
+from flask import Flask, jsonify
+import os
+from hn import HN
+import redis
+
+hn = HN()
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+app = Flask(__name__)
+
+
+def news():
+    hiren = []
+    nisha = {}
+    for story in hn.get_stories(story_type='', limit=30):
+        if not r.get(story.title):
+            r.setex(story.title, 100, "Hi hiren ! :D")
+            hiren.append((story.title, story.link, story.comments_link))
+        else:
+            pass
+    nisha['hiren'] = hiren
+    return nisha
+
+
+@app.route('/')
+def index():
+    #return "X"
+    return jsonify(news())
+
+
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
