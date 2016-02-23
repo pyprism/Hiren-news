e0856b7e0d11a320532a06742fbdddf759c37e45
Ashutosh Das <git.pyprism@gmail.com>
2016-02-23 23:13:07 +0600

    added rss parser

---
+++ hn/rss.py
@@ -0,0 +1,17 @@
+import feedparser
+from db.models import Content
+from hiren import db
+
+
+def rss():
+    feed = []
+    bunny = feedparser.parse('https://news.ycombinator.com/rss')
+    for i in bunny.entries:
+        duplicate = Content.query.filter_by(comments_url=i.comments).first()
+        if duplicate is None:
+            feed.append(i.title_detail.value + ' :' + i.link + " " + "Comments: " + i.comments)
+            hiren = Content(i.comments)
+            db.session.add(hiren)
+            db.session.commit()
+    return feed
+
