7945b6875db06e2e37d2690ed62d9f17134df49f
Ashutosh Das <git.pyprism@gmail.com>
2016-02-24 19:41:13 +0600

    fixed strings format

--- hn/rss.py
+++ hn/rss.py
@@ -4,12 +4,16 @@ from hiren import db
 
 
 def rss():
+    """
+    Call hacker news api
+    :return: List of posts
+    """
     feed = []
     bunny = feedparser.parse('https://news.ycombinator.com/rss')
     for i in bunny.entries:
         duplicate = Content.query.filter_by(comments_url=i.comments).first()
         if duplicate is None:
-            feed.append(i.title_detail.value + ' :' + i.link + " " + "Comments: " + i.comments)
+            feed.append(i.title_detail.value + ' : ' + i.link + " " + "Comments: " + i.comments)
             hiren = Content(i.comments)
             db.session.add(hiren)
             db.session.commit()
