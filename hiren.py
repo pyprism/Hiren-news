039cc136f343d4dee6b34961c295d352449a2ea9
Ashutosh Das <git.pyprism@gmail.com>
2016-02-24 19:40:51 +0600

    updated database schema, added json file

--- hiren.py
+++ hiren.py
@@ -2,9 +2,21 @@ from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
+import json
+
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+
+try:
+    with open(os.path.join(SITE_ROOT, 'config.local.json')) as f:
+        JSON_DATA = json.load(f)
+except FileNotFoundError:
+    with open(os.path.join(SITE_ROOT, 'config.json')) as f:
+        JSON_DATA = json.load(f)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + JSON_DATA['db_user'] + ':'\
+                                        + JSON_DATA['db_pass'] + '@localhost/' + JSON_DATA['db_name']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
