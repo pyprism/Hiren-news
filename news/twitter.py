7170f6fe0cf6559d4f96b2aa60d8973e0fe46d0c
Ashutosh Das <git.pyprism@gmail.com>
2016-02-27 13:20:36 +0600

    fixed issue

--- news/twitter.py
+++ news/twitter.py
@@ -5,12 +5,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiren.settings")
 django.setup()
 
 from hiren.settings import JSON_DATA
-from hn import rss
+from .hn import rss
 import tweepy
-from celery import shared_task
 
 
-@shared_task()
 def api():
     """
     Post status(s) to twitter
@@ -18,6 +16,6 @@ def api():
     auth = tweepy.OAuthHandler(JSON_DATA['consumer_key'], JSON_DATA['consumer_secret'])
     auth.set_access_token(JSON_DATA['access_token'], JSON_DATA['access_token_secret'])
     api = tweepy.API(auth)
-    for i in rss.rss():
+    for i in rss():
         api.update_status(i)
 
