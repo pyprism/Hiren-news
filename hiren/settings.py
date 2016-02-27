7a2d266ca6748f89cfdd2dcf15d08b74a7bbb5f7
Ashutosh Das <git.pyprism@gmail.com>
2016-02-27 11:35:53 +0600

    added celery conf

--- hiren/settings.py
+++ hiren/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import json
+from celery.schedules import crontab
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -44,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news'
+    'news',
+    'kombu.transport.django',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -144,3 +147,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BROKER_URL = 'django://'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERYBEAT_SCHEDULE = {
+    'run-every-5-minutes': {
+        'task': 'news.twitter.api',
+        'schedule': crontab(minute='*/5'),
+    },
+}
