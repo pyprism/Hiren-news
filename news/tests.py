82c5d6911bbdf9f5f8beddff97c69c785dcb65f7
Ashutosh Das <git.pyprism@gmail.com>
2016-04-17 12:07:57 +0600

    added model test

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
+from .models import Bunny, Tag
 
-# Create your tests here.
+
+class ModelTest(TransactionTestCase):
+    """
+    Test all models
+    """
+    reset_sequences = True
+
+    def setUp(self):
+        Bunny.objects.create(comment_url='https://news.ycombinator.com/item?id=11512455')
+        Bunny.objects.create(comment_url='https://news.ycombinator.com/item?id=11511062')
+        Tag.objects.create(name='Django')
+        Tag.objects.create(name='python')
+
+    def test_tag_model(self):
+        tag_item = Tag.objects.all()
+        self.assertEqual(tag_item.count(), 2)
+
+        obj = Tag.objects.get(name='Django')
+        obj.delete()
+
+        tag_item = Tag.objects.all()
+        self.assertEqual(tag_item.count(), 1)
+
+    def test_bunny_model(self):
+        bunny_item = Bunny.objects.all()
+        self.assertEqual(bunny_item.count(), 2)
+
+        obj = Bunny.objects.get(comment_url='https://news.ycombinator.com/item?id=11512455')
+        obj.delete()
+
+        bunny_item = Bunny.objects.all()
+        self.assertEqual(bunny_item.count(), 1)
