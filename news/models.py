bd669bb6bc75175693870108594d9f1dd5c9125f
Ashutosh Das <git.pyprism@gmail.com>
2016-04-17 11:21:41 +0600

    added new model

--- news/models.py
+++ news/models.py
@@ -7,3 +7,9 @@ class Bunny(models.Model):
 
     def __str__(self):
         return 'Comment={0}, Date={1}'.format(self.comment_url, self.time)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
