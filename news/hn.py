eff12940a297c16344b57b378224d6e95dfb0ed1
Ashutosh Das <git.pyprism@gmail.com>
2016-04-18 19:46:33 +0600

    added hash support :smile:

--- news/hn.py
+++ news/hn.py
@@ -1,11 +1,12 @@
 import feedparser
 import django
 import os
+import re
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiren.settings")
 django.setup()
 
-from news.models import Bunny
+from news.models import Bunny, Tag
 
 
 def rss():
@@ -14,10 +15,18 @@ def rss():
     :return: List of posts
     """
     feed = []
+    tags = Tag.objects.all()
     bunny = feedparser.parse('https://news.ycombinator.com/rss')
     for i in bunny.entries:
         duplicate = Bunny.objects.filter(comment_url=i.comments)
         if duplicate.exists() is False:
-            feed.append(i.title_detail.value + ' : ' + i.link + " " + "Comments: " + i.comments)
+            _hash = ''
+            for bunny in tags:    # search for predefined value in post title
+                if re.match('\\b' + bunny.name + '\\b', i.title_detail.value, re.I):
+                    _hash = _hash + ' #' + bunny.name
+            if len(_hash):   # if _hash has value append to end of the post
+                feed.append(i.title_detail.value + ' : ' + i.link + _hash + " " + "Comments: " + i.comments)
+            else:
+                feed.append(i.title_detail.value + ' : ' + i.link + " " + "Comments: " + i.comments)
             Bunny(comment_url=i.comments).save()
     return feed
