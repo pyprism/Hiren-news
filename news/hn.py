6fc592d2ab8ca48188f92b0060d2f92ea5e84e16
Ashutosh Das <git.pyprism@gmail.com>
2016-04-19 16:13:53 +0600

    added bitly

--- news/hn.py
+++ news/hn.py
@@ -2,11 +2,23 @@ import feedparser
 import django
 import os
 import re
+import requests
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiren.settings")
 django.setup()
 
 from news.models import Bunny, Tag
+from hiren.settings import JSON_DATA
+
+
+def bitly(url):
+    """
+    Call Bit.ly api
+    :return: the fu$king short url
+    """
+    payload = {'access_token': JSON_DATA['bitly_access_token'], 'longUrl': url, 'format': 'txt'}
+    response = requests.get('https://api-ssl.bitly.com/v3/shorten', params=payload)
+    return response.text
 
 
 def rss():
@@ -25,8 +37,8 @@ def rss():
                 if re.match('\\b' + bunny.name + '\\b', i.title_detail.value, re.I):
                     _hash = _hash + ' #' + bunny.name
             if len(_hash):   # if _hash has value append to end of the post
-                feed.append(i.title_detail.value + ' : ' + i.link + _hash + " " + "Comments: " + i.comments)
+                feed.append(i.title_detail.value + ' : ' + bitly(i.link) + _hash + " " + "Comments: " + bitly(i.comments))
             else:
-                feed.append(i.title_detail.value + ' : ' + i.link + " " + "Comments: " + i.comments)
+                feed.append(i.title_detail.value + ' : ' + bitly(i.link) + " " + "Comments: " + bitly(i.comments))
             Bunny(comment_url=i.comments).save()
     return feed
