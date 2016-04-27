aaac057953c41954f67e3ab8b60a0e1f13802ca4
Ashutosh Das <git.pyprism@gmail.com>
2016-04-27 20:29:12 +0600

    refactored

--- news/posts.py
+++ news/posts.py
@@ -10,31 +10,26 @@ import tweepy
 import facebook
 
 
-posts_collection = rss()
-
-
-def twitter_api():
+def posts():
     """
-    Post status(s) to twitter
+    Post status(s) to fb and twitter
+    :return:
     """
     auth = tweepy.OAuthHandler(JSON_DATA['consumer_key'], JSON_DATA['consumer_secret'])
     auth.set_access_token(JSON_DATA['access_token'], JSON_DATA['access_token_secret'])
     api = tweepy.API(auth, wait_on_rate_limit=True)
-    for i in posts_collection:
+
+    graph = facebook.GraphAPI(access_token=JSON_DATA['fb_page_token'], version='2.5')  # access_token= http://stackoverflow.com/questions/17197970/facebook-permanent-page-access-token
+
+    for i in rss():
         try:
-            api.update_status(i)
+            api.update_status(i['message'])
         except Exception as e:
             print(e, i)
 
-
-def facebook_api():
-    """
-    Post status to fb page
-    """
-    graph = facebook.GraphAPI(access_token=JSON_DATA['fb_page_token'], version='2.5')  # access_token= http://stackoverflow.com/questions/17197970/facebook-permanent-page-access-token
-    for i in posts_collection:
         try:
-            res = graph.put_object(JSON_DATA['fb_page'], 'feed', message=i)
+            res = graph.put_object(JSON_DATA['fb_page'], 'feed', message=i['message'], link=i['link'])
         except Exception as e:
             print(res, e)
+
 
