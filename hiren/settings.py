5e88f25be1dbb9190f6eb729c4be9efd47ce8daf
Ashutosh Das <git.pyprism@gmail.com>
2016-10-08 11:07:41 +0600

    removed celery

--- hiren/settings.py
+++ hiren/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import json
-from celery.schedules import crontab
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
