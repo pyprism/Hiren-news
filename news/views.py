c3bb452b1e9b63b0bcc2556dc71b5f673bb6b6a2
Ashutosh Das <git.pyprism@gmail.com>
2017-04-10 12:27:35 +0600

    added experimental scheduler

--- news/views.py
+++ news/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from news.models import Bunny
+from .posts import posts
 
 
 def index(request):
+    """
+    Returns number of db row
+    :param request:
+    :return:
+    """
     return HttpResponse('Number of Bunny(s): ' + str(Bunny.objects.all().count()))
+
+def scheduler(request):
+    """
+    Just a simple golang-python hybrid scheduler :P lol !
+    :param request:
+    :return:
+    """
+    #posts()
+    print('sssss')
+    return HttpResponse("Hello Hiren :D !")
