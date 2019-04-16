653cab1a8d4824ec6531035647d8a455ef9c8c38
Ashutosh Das <git.pyprism@gmail.com>
2019-04-16 23:12:14 +0600

    fixed routes

--- hiren/urls.py
+++ hiren/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import re_path
+from django.urls import path, re_path,
 from django.contrib import admin
-from news.views import index
+from news.views import index, scheduler
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
-    re_path(r'^', index),
+    path('scheduler/', scheduler),
+    path(r'', index),
 ]
