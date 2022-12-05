b01d2dd928c98a3655e9e4f36d92bc2c37dc52fa
Ashutosh Das <git.pyprism@gmail.com>
2022-12-05 09:49:38 +0600

    removed twitter api call

--- news/posts.py
+++ news/posts.py
@@ -12,21 +12,12 @@ import facebook
 
 def posts():
     """
-    Post status(s) to fb and twitter
+    Post status(s) to fb
     :return:
     """
-    auth = tweepy.OAuthHandler(JSON_DATA['consumer_key'], JSON_DATA['consumer_secret'])
-    auth.set_access_token(JSON_DATA['access_token'], JSON_DATA['access_token_secret'])
-    api = tweepy.API(auth, wait_on_rate_limit=True)
-
-    graph = facebook.GraphAPI(access_token=JSON_DATA['fb_page_token'], version='3.1')  # access_token= http://stackoverflow.com/questions/17197970/facebook-permanent-page-access-token
+    graph = facebook.GraphAPI(access_token=JSON_DATA['fb_page_token'], version='15.0')  # access_token= http://stackoverflow.com/questions/17197970/facebook-permanent-page-access-token
 
     for i in rss():
-        try:
-            api.update_status(i['message'])
-        except Exception as e:
-            print(e, i)
-
         try:
             graph.put_object(JSON_DATA['fb_page'], 'feed', message=i['message'], link=i['link'])
         except Exception as e:
