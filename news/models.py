61dd990f6c39bbef4d20983273ff2d321756f58e
Ashutosh Das <git.pyprism@gmail.com>
2023-09-04 22:20:34 +0600

    added new column

--- news/models.py
+++ news/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Bunny(models.Model):
     comment_url = models.URLField(unique=True)
-    time = models.DateTimeField(auto_now_add=True)
+    time = models.DateTimeField(auto_now_add=True)   # save time when post is saved
+    posted = models.BooleanField(default=False)
 
 
 class Tag(models.Model):
