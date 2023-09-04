cc32efb994211ec43fc38c4a08c86911b631c3a5
Ashutosh Das <git.pyprism@gmail.com>
2023-09-04 22:21:06 +0600

    refactored posting: removed sdk, added raw api calling

--- news/posts.py
+++ news/posts.py
@@ -1,22 +1,29 @@
 import django
 import os
 
+from .models import Bunny
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiren.settings")
 django.setup()
 
 from hiren.settings import JSON_DATA
 from .hn import rss
-import facebook
+import requests
 
 
 def posts():
     """
     Post status(s) to fb
+    get permanent access_token= http://stackoverflow.com/questions/17197970/facebook-permanent-page-access-token
     :return:
     """
-    graph = facebook.GraphAPI(access_token=JSON_DATA['fb_page_token'], version='3.1')  # access_token= http://stackoverflow.com/questions/17197970/facebook-permanent-page-access-token
+    hn_posts = Bunny.objects.filter(posted=False)[:5]
+    for i in hn_posts:
+        response = requests.post(f"https://graph.facebook.com/{JSON_DATA['fb_page']}/feed?message={i['message']}&link={i['link']}&access_token={JSON_DATA['fb_page_token']}")
+        if response.status_code == 200:
+            Bunny.objects.filter(comment_url=i.comment_url).update(posted=True)
+        else:
+            raise Exception("Error while posting to fb")
 
-    for i in rss():
-        graph.put_object(JSON_DATA['fb_page'], 'feed', message=i['message'], link=i['link'])
 
 
