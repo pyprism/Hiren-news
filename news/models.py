f09a3fd6c736f0027c77e71e3b7a9192cf072cce
Ashutosh Das <git.pyprism@gmail.com>
2023-09-04 23:38:20 +0600

    changed name

--- news/models.py
+++ news/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Bunny(models.Model):
-    comment_url = models.URLField(unique=True)
+    title = models.TextField()
+    main_url = models.URLField(unique=True)
     time = models.DateTimeField(auto_now_add=True)   # save time when post is saved
     posted = models.BooleanField(default=False)
 
