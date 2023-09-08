12fd6a7737ffc9cdf04b005046f8ad59ab332729
Ashutosh Das <git.pyprism@gmail.com>
2023-09-09 00:01:15 +0600

    fixed static root

--- hiren/settings.py
+++ hiren/settings.py
@@ -153,7 +153,7 @@ STATICFILES_FINDERS = (
 )
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/"),
 )
 STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
 
